985c2a9efd8e902343394a434ed87422e9e61c27
Jason Rowberry <jman98565@gmail.com>
2024-02-29 08:46:28 -0700

    It gets the score, has a series of if/elif/else, and prints grade and a message

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage");
+        string score_str = Console.ReadLine();
+        int score = int.Parse(score_str);
+        string letter_grade = "";
+        if (score >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (score >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if (score >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if (score >= 60)
+        {
+            letter_grade = "D";
+        }
+        else
+        {
+            letter_grade = "F";
+        }
+        Console.WriteLine($"You got a(n) {letter_grade}");
+        if (score >= 70)
+        {
+            Console.WriteLine("You have passed, Congragulations");
+        }
+        else
+        {
+            Console.WriteLine("You have failed, Better luck next time");
+        }
     }
 }
