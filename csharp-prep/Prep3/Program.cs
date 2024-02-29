8a925d34c90faf1a50b76ba1843e11028d5a0168
Jason Rowberry <jman98565@gmail.com>
2024-02-29 10:44:59 -0700

    Established a do-while to guess the number, added RNG

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomnumber = new Random();
+        int num = randomnumber.Next(1,100);
+        
+        int guess;
+        guess = -1;
+        
+        do
+        {
+        Console.Write("Guess a number");
+        string guess_str = Console.ReadLine();
+        guess = int.Parse(guess_str);
+        if (guess > num)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < num)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess == num)
+        {
+            Console.WriteLine("You guessed it!");
+            num = -9999999;
+
+        }
+        }
+        while (num != -9999999);
     }
 }
