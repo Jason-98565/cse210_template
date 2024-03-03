574e6cb6bcbe47d9af3487d7ab66df3c8302f7ac
Jason Rowberry <jman98565@gmail.com>
2024-03-02 21:16:35 -0700

    It asks for a name and a number and returns a sentence that includes that name and the square of the number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        {
+            display_message();
+            string name = get_user_name();
+            int fav_number = get_fav_number();
+
+            int squared_number = square_number(fav_number);
+
+            display_results(name,squared_number);
+        }
+        static void display_message()
+        {
+            Console.WriteLine("Welcome to the Program");
+        }
+        static string get_user_name()
+        {
+            Console.Write("Please Enter your name");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int get_fav_number()
+        {
+            Console.Write("Please enter your favorite number");
+            string fav_number_str = Console.ReadLine();
+            int fav_number = int.Parse(fav_number_str);
+            return fav_number;
+        }
+        static int square_number(int fav_number)
+        {
+            int squared_number = fav_number*fav_number;
+            return squared_number;
+        }
+        static void display_results(string name, int squared_number)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squared_number}");
+        }
     }
 }
